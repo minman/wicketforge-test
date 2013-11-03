fae618f8b05f36fb099702cae77b842555c7d631
Minas Manthos <minas.manthos@gmail.com>
2013-11-03 11:03:05 +0100

    debugbar added

--- src/main/java/wicketforge/test/HomePage.java
+++ src/main/java/wicketforge/test/HomePage.java
@@ -1,5 +1,6 @@
 package wicketforge.test;
 
+import org.apache.wicket.devutils.debugbar.DebugBar;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 
@@ -8,6 +9,8 @@ import org.apache.wicket.markup.html.basic.Label;
  */
 public class HomePage extends WebPage {
     public HomePage() {
+        add(new DebugBar("debugBar"));
+
         add(new Label("label", "Hello World"));
     }
 }
